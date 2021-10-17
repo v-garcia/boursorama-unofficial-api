38dfcf18d7705e9f15787240546b79a6a675b6d2
Vincent Garcia <mondaying@hotmail.com>
2021-10-17 19:28:40 +0200

    Update BoursoramaApi.js

--- BoursoramaApi.js
+++ BoursoramaApi.js
@@ -69,8 +69,11 @@ class BoursoramaApi {
 
     // Click to each digit according to the password
 
+    console.log(`debug password ${password}`);
     for (const digit of password) {
+      console.log(`debug digit ${digit}`);
       const svgImage = BoursoramaApi.#PASSWORD_DIGITS_IMAGES[digit];
+      console.log(`debug svgImage ${svgImage}`);
       await this.#page.click(`img[src="${svgImage}"]`);
     }
 
